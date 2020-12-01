d9933f7f5866021f939602b3145891dda015f647
schlöpp! <67605203+schlopp@users.noreply.github.com>
2020-12-01 14:35:58 +0100

    jquery + copy paste thingies
    quality of life shit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function jekkrmoeder(){
-  website = prompt("Enter a website\n('https://youtube.com', etc)")
-  window.open(website, "", "width=1000,height=1000")
-}
+$(document).ready(function(){
+
+
+  function jekkrmoeder(){
+    website = prompt("Enter a website\n('https://youtube.com', etc)")
+    window.open(website, "", "width=1000,height=1000")
+  }
+
+  $(".links span").on('click', function(){
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(this).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert('copied!');
+  });
+
+});
