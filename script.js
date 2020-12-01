add0b2f12ccb08347fd9098f14031405f661c767
schlöpp! <67605203+schlopp@users.noreply.github.com>
2020-12-01 15:15:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
 
-  function jekkrmoeder(){
-    website = prompt("Enter a website\n('https://youtube.com', etc)")
-    window.open(website, "", "width=1000,height=1000")
-  }
+  $("#go_undercover").on('click', function(){
+    website = prompt("Enter a website\n('https://youtube.com', etc)");
+    window.open(website, "", "width=1000,height=1000");
+  });
 
   $(".links span").on('click', function(){
     var $temp = $("<input>");
